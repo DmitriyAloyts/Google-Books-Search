acb1d92b8ec0e0017b0faf35dcd85db9af04185a
Dmitriy A <dmitriy@iinet.net.au>
2020-04-05 22:39:20 +1000

    initial load

---
+++ client/build/precache-manifest.d546dd5e996d25b3bbddfc784cdfb6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "954c4b99a64bb8d3bd23",
+    "url": "/static/css/main.81d8b299.chunk.css"
+  },
+  {
+    "revision": "954c4b99a64bb8d3bd23",
+    "url": "/static/js/main.fdca744a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4e051c3e7b236cd20c48",
+    "url": "/static/css/2.647aa24d.chunk.css"
+  },
+  {
+    "revision": "4e051c3e7b236cd20c48",
+    "url": "/static/js/2.947a4c27.chunk.js"
+  },
+  {
+    "revision": "ffe8a8432388235f4e23635d73b9617a",
+    "url": "/index.html"
+  }
+];
